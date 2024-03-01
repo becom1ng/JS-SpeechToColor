a3bf9ac22f804ab551835aecf1e8ded05603b5e9
becom1ng <becom1ng@users.noreply.github.com>
2024-03-01 12:00:49 -0600

    Working speech rec plus button events

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+
 const SpeechRecognition =
 	window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const rec = new SpeechRecognition();
 
 rec.lang = 'en-US';
-rec.continuous = false;
+rec.continuous = true;
 
 rec.onresult = function (e) {
-	console.log(e.results);
+	for (let i = e.resultIndex; i < e.results.length; i++) {
+		const script = e.results[i][0].transcript.toLowerCase().trim();
+
+		if (isValidColor(script)) {
+			document.body.style.backgroundColor = script;
+		} else {
+			alert('Try again! Please say a color.');
+		}
+	}
 };
 
-rec.start();
+function isValidColor(strColor) {
+	var s = new Option().style;
+	s.color = strColor;
+
+	// return 'false' if color wasn't assigned
+	return s.color == strColor;
+}
+
+start.addEventListener('click', () => {
+	start.classList.add('large');
+	stop.classList.add('small');
+	rec.start();
+});
+stop.addEventListener('click', () => {
+	start.classList.remove('large');
+	stop.classList.remove('small');
+	stop.classList.add('stopclick');
+	setTimeout(() => {
+		stop.classList.remove('stopclick');
+	}, 100);
+	rec.abort();
+});
